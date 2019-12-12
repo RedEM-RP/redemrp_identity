503da671e47a7210df911deb646e3d1cd2065082
kanersps <12296463+kanersps@users.noreply.github.com>
2019-12-12 21:09:43 +0100

    Change default to true

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 Config.CameraLocation = {x=-1921.78, y=105.54, z=338.37}
 
-Config.UsingRespawn = false -- If you are using RedEM-RP Respawn, set this to 'true' otherwise, keep it false and set "Config.SpawnPoint" to where you want players to spawn. (https://github.com/RedEM-RP/redemrp_respawn)
+Config.UsingRespawn = true -- If you are using RedEM-RP Respawn, set this to 'true' otherwise, keep it false and set "Config.SpawnPoint" to where you want players to spawn. (https://github.com/RedEM-RP/redemrp_respawn)
 Config.SpawnPoint = {x=-908.72, y=-1366.0, z=45.41}
 
 
