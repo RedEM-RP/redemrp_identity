75ef1dc219a2ad9fc31290da37453d5bea76083a
Ktoś <43894510+Ktos93@users.noreply.github.com>
2020-07-02 17:44:52 +0200

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 client_scripts {
-	'config.lua',
 	'client/cl_main.lua',
+	'config.lua'
 }
 
 server_scripts {
@@ -16,6 +16,7 @@ files{
 'html/crock.ttf',
 'html/style.css',
 'html/ui.html',
+'html/circle.gif',
 'html/js/jquery-1.4.1.min.js',
 'html/js/jquery-func.js',
 'html/js/jquery.jcarousel.pack.js',
--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,15 +1,7 @@
 local identifierUsed = GetConvar('es_identifierUsed', 'steam')
 
-RegisterServerEvent("redemrp_identity:updateName")
-AddEventHandler("redemrp_identity:updateName", function(fname, lname)
-    TriggerEvent('redemrp:getPlayerFromId', source, function(user)
-        user.setFirstname(tostring(fname))
-        user.setLastname(tostring(lname))
-    end)
-end)
-
 RegisterServerEvent("redemrp_identity:getCharacters")
-AddEventHandler("redemrp_identity:getCharacters", function() 
+AddEventHandler("redemrp_identity:getCharacters", function()
     local _source = source
     local id
     for k,v in ipairs(GetPlayerIdentifiers(_source))do
@@ -18,14 +10,21 @@ AddEventHandler("redemrp_identity:getCharacters", function()
             break
         end
     end
-	
-	MySQL.Async.fetchAll('SELECT * FROM characters WHERE `identifier`=@identifier;', {identifier = id}, function(result)
-	    TriggerClientEvent('redemrp_identity:getCharacters', _source, result)
-	end)
-end)
 
-RegisterServerEvent("redemrp_identity:deleteCharacter")
-AddEventHandler("redemrp_identity:deleteCharacter", function(_charid, Callback) 
+    MySQL.Async.fetchAll('SELECT * FROM characters WHERE `identifier`=@identifier;', {identifier = id}, function(result)
+        MySQL.Async.fetchAll('SELECT * FROM skins WHERE `identifier`=@identifier;', {identifier = id}, function(result2)
+            MySQL.Async.fetchAll('SELECT * FROM clothes WHERE `identifier`=@identifier;', {identifier = id}, function(result3)
+                TriggerClientEvent('redemrp_identity:openSelectionMenu', _source, result, result2, result3)
+
+
+            end)
+        end)
+    end)
+
+
+
+end)RegisterServerEvent("redemrp_identity:deleteCharacter")
+AddEventHandler("redemrp_identity:deleteCharacter", function(_charid, Callback)
     local _source = source
     local id
     for k,v in ipairs(GetPlayerIdentifiers(_source))do
@@ -34,10 +33,85 @@ AddEventHandler("redemrp_identity:deleteCharacter", function(_charid, Callback)
             break
         end
     end
-	
-	MySQL.Async.fetchAll('DELETE FROM characters WHERE `identifier` = @identifier AND `characterid`=@characterid;', {identifier = id, characterid=_charid}, function(result)
-		if result then
-		else
-		end
-	end)
+
+    MySQL.Async.fetchAll('DELETE FROM characters WHERE `identifier` = @identifier AND `characterid`=@characterid;', {identifier = id, characterid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+    MySQL.Async.fetchAll('DELETE FROM horses WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+    MySQL.Async.fetchAll('DELETE FROM player_animals WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+    MySQL.Async.fetchAll('DELETE FROM status WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+
+    MySQL.Async.fetchAll('DELETE FROM coaches WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+
+    MySQL.Async.fetchAll('DELETE FROM horse_components WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+    MySQL.Async.fetchAll('DELETE FROM mail_cases WHERE `owner` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+    MySQL.Async.fetchAll('DELETE FROM notebook WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+    MySQL.Async.fetchAll('DELETE FROM player_animals WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+    MySQL.Async.fetchAll('DELETE FROM user_locker WHERE `identifier` = @identifier AND `charid`=@charid;', {identifier = id, charid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
+    TriggerEvent('redemrp_db:getCurrentGang', id, _charid, function(user)
+        if user ~= false  and tonumber(user.ganggrade) >= 10 then
+            MySQL.Async.fetchAll('DELETE FROM gang WHERE `gang` = @gang;', {gang = user.gang}, function(result)
+                if result then
+                else
+                end
+            end)
+            MySQL.Async.fetchAll('DELETE FROM gang_locations WHERE `gang` = @gang;', {gang = user.gang}, function(result)
+                if result then
+                else
+                end
+            end)
+        end
+    end)
+    Wait(1000)
+    MySQL.Async.fetchAll('DELETE FROM gang WHERE `identifier` = @identifier AND `characterid`=@characterid;', {identifier = id, characterid=_charid}, function(result)
+        if result then
+        else
+        end
+    end)
+
 end)
